f7f4dee28815f82829fc9c19e7a2e13aa95d2b18
Jenna Hartley <jenna.hartley@pushinteractions.com>
2021-01-17 17:31:15 -0600

    Exercise 3.9

--- index.js
+++ index.js
@@ -96,6 +96,19 @@ app.get('/api/persons/:id', (req, res) => {
     }
 })
 
+app.put('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const personToUpdate = persons.find(person => person.id === id)
+
+    if (personToUpdate) {
+        personToUpdate.name = req.body.name
+        personToUpdate.number = req.body.number
+        res.json(personToUpdate)
+    } else {
+        res.status(404).end()
+    }
+})
+
 app.delete('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
     persons = persons.filter(person => person.id !== id)
